f2149d9c639989711c2dc550c845adf6d83f9b6e
Rutam Bhagat <rutambhagat@gmail.com>
2025-07-20 02:16:10 +0530

    feat: define schema for messages and events in Convex data model

---
+++ convex/schema.ts
@@ -0,0 +1,16 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  messages: defineTable({
+    user: v.string(),
+    body: v.string(),
+  })
+    .index("by_user", ["user"]),
+
+  events: defineTable({
+    user: v.string(),
+    message: v.string(),
+  })
+    .index("by_user", ["user"]),
+});
